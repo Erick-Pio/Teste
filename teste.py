5b6c664e72c4f9de327b132a3d12853df418bd6a
erick-pio <erick.pio@sptech.school>
2022-12-06 15:44:37 -0300

    Deus certo

--- teste.py
+++ teste.py
@@ -19,6 +19,8 @@ print(os.path.dirname(os.path.abspath(__file__)))
 
 df = pd.read_excel("C:/Users/Aluno/Downloads/Teste/eKran.xlsm")
 SP = df.loc[df['Estado'] == 'São Paulo']
+#RJ = df.loc[df['Estado'] == 'Rio de Janeiro']
+#AP = df.loc[df['Estado'] == 'Amapá']
     
 cols = ['ID do pedido', 'Empresa','Estado', 'Lucro']
 SP.drop(cols, axis=1, inplace=True)
@@ -37,3 +39,22 @@ y.plot(figsize=(15,6))
 
 plt.savefig('my_plot.png')
 
+from pylab import rcParams
+rcParams['figure.figsize'] = 18, 8
+decomposition = sm.tsa.seasonal_decompose(y, model='additive')
+ig = decomposition.plot()
+plt.savefig('plot2.png')
+
+p = d = q = range(0, 2)
+pdq = list(itertools.product(p, d, q))
+seasonal_pdq = [(x[0], x[1], x[2], 12) for x in list(itertools.product(p, d, q))]
+
+mod = sm.tsa.statespace.SARIMAX(y,
+                               order=(1, 1, 1),
+                               seasonal_order=(1, 1, 0, 12),
+                               enforce_invertibility=False)
+results = mod.fit()
+print(results.summary().tables[1])
+
+results.plot_diagnostics(figsize=(16, 8))
+plt.savefig('plot3.png')
